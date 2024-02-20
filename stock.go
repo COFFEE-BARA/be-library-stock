d8334b1e885fece837ec8503e0198393abe54a0a
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2024-02-20 15:50:00 +0900

    Fix : lambda 에서 load env 에러 해결

--- stock.go
+++ stock.go
@@ -63,6 +63,25 @@ func EventHandler(ctx context.Context, request events.APIGatewayProxyRequest) (e
 		log.Fatal("Error loading .env file")
 	}
 
+	CLOUD_ID := os.Getenv("CLOUD_ID")
+	API_KEY := os.Getenv("API_KEY")
+	INDEX_NAME := os.Getenv("INDEX_NAME")
+	FIELD_NAME := os.Getenv("FIELD_NAME")
+	REGION := os.Getenv("REGION")
+	TABLE_NAME := os.Getenv("TABLE_NAME")
+
+	AUTH_KEY_SH := os.Getenv("AUTH_KEY_SH")
+	AUTH_KEY_YG := os.Getenv("AUTH_KEY_YG")
+	AUTH_KEY_YJ := os.Getenv("AUTH_KEY_YJ")
+	AUTH_KEY_DY := os.Getenv("AUTH_KEY_DY")
+
+	// 호출이 안되면 다른 auth_key로 두기
+	var authKeyList []string
+	authKeyList = append(authKeyList, AUTH_KEY_SH)
+	authKeyList = append(authKeyList, AUTH_KEY_YG)
+	authKeyList = append(authKeyList, AUTH_KEY_YJ)
+	authKeyList = append(authKeyList, AUTH_KEY_DY)
+
 	// Set CORS headers
 	headers := map[string]string{
 		"Access-Control-Allow-Origin":  "*", // Allow requests from any origin
@@ -118,32 +137,32 @@ func EventHandler(ctx context.Context, request events.APIGatewayProxyRequest) (e
 
 	//3. escloud에서 책이름 가져오기
 
-	esClient, err := connectElasticSearch(os.Getenv("CLOUD_ID"), os.Getenv("API_KEY"))
+	esClient, err := connectElasticSearch(CLOUD_ID, API_KEY)
 	if err != nil {
 		fmt.Println("Error connecting to Elasticsearch:", err)
 		return events.APIGatewayProxyResponse{StatusCode: 500, Headers: headers}, err
 	}
 	//3.1 isbn 값으로 검색하기
-	title, err := searchTitle(esClient, os.Getenv("INDEX_NAME"), os.Getenv("FIELD_NAME"), isbn)
+	title, err := searchTitle(esClient, INDEX_NAME, FIELD_NAME, isbn)
 	if err != nil {
 		fmt.Println("인덱스 검색 중 오류 발생:", err)
 		return events.APIGatewayProxyResponse{StatusCode: 500, Headers: headers}, err
 	}
 
 	//4. dynamoDB에서 도서관정봅가져오기
-	sess, err := createNewSession()
+	sess, err := createNewSession(REGION)
 	if err != nil {
 		log.Println("Error creating session:", err)
 		return events.APIGatewayProxyResponse{StatusCode: 500, Headers: headers}, err
 	}
-	result, err := scanDynamoDB(sess)
+	result, err := scanDynamoDB(sess, TABLE_NAME)
 	if err != nil {
 		log.Println(err)
 		return events.APIGatewayProxyResponse{StatusCode: 500, Headers: headers}, err
 	}
 
 	//5. 도서관 api 돌려서 대출가능한 도서관 가져오기
-	libraries, err := libraryHandler(result, location, isbn)
+	libraries, err := libraryHandler(result, location, isbn, authKeyList)
 	if err != nil {
 		log.Println(err)
 		return events.APIGatewayProxyResponse{StatusCode: 500, Headers: headers}, err
@@ -236,10 +255,10 @@ func searchTitle(es *elasticsearch.Client, indexName, fieldName, value string) (
 
 }
 
-func createNewSession() (*session.Session, error) {
+func createNewSession(REGION string) (*session.Session, error) {
 
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(os.Getenv("REGION")),
+		Region: aws.String(REGION),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("error scanning createNewSession: %v", err)
@@ -247,9 +266,9 @@ func createNewSession() (*session.Session, error) {
 	return sess, nil
 }
 
-func scanDynamoDB(sess *session.Session) (*dynamodb.ScanOutput, error) {
+func scanDynamoDB(sess *session.Session, TABLE_NAME string) (*dynamodb.ScanOutput, error) {
 	svc := dynamodb.New(sess)
-	tableName := os.Getenv("TABLE_NAME")
+	tableName := TABLE_NAME
 
 	input := &dynamodb.ScanInput{
 		TableName: aws.String(tableName),
@@ -263,7 +282,7 @@ func scanDynamoDB(sess *session.Session) (*dynamodb.ScanOutput, error) {
 	return result, nil
 }
 
-func libraryHandler(result *dynamodb.ScanOutput, location Location, isbn string) ([]LibraryInfo, error) {
+func libraryHandler(result *dynamodb.ScanOutput, location Location, isbn string, authKeyList []string) ([]LibraryInfo, error) {
 	var libraries []LibraryInfo
 	for _, item := range result.Items {
 		libCode := *item["libCode"].S
@@ -287,7 +306,7 @@ func libraryHandler(result *dynamodb.ScanOutput, location Location, isbn string)
 	}
 
 	//var lib []LibraryInfo
-	lib, err := callAPIs(libraries, isbn)
+	lib, err := callAPIs(libraries, isbn, authKeyList)
 	if err != nil {
 		return nil, err
 	}
@@ -321,13 +340,7 @@ func calculateDistance(location Location, latitude string, longitude string) flo
 	return distance
 }
 
-func callAPI(libCode string, isbn string) (bool, error) {
-	// 호출이 안되면 다른 auth_key로 두기
-	var authKeyList []string
-	authKeyList = append(authKeyList, os.Getenv("AUTH_KEY_SH"))
-	authKeyList = append(authKeyList, os.Getenv("AUTH_KEY_YG"))
-	authKeyList = append(authKeyList, os.Getenv("AUTH_KEY_YJ"))
-	authKeyList = append(authKeyList, os.Getenv("AUTH_KEY_DY"))
+func callAPI(libCode string, isbn string, authKeyList []string) (bool, error) {
 
 	for _, authKey := range authKeyList {
 		apiURL := fmt.Sprintf("https://data4library.kr/api/bookExist?authKey=%s&libCode=%s&isbn13=%s", authKey, libCode, isbn)
@@ -362,7 +375,7 @@ func callAPI(libCode string, isbn string) (bool, error) {
 
 }
 
-func callAPIs(libraries []LibraryInfo, isbn string) ([]LibraryInfo, error) {
+func callAPIs(libraries []LibraryInfo, isbn string, authKeyList []string) ([]LibraryInfo, error) {
 	ch := make(chan LibraryInfo)
 
 	var wg sync.WaitGroup
@@ -372,7 +385,7 @@ func callAPIs(libraries []LibraryInfo, isbn string) ([]LibraryInfo, error) {
 		go func(lib LibraryInfo) error {
 			defer wg.Done()
 
-			flag, err := callAPI(lib.LibCode, isbn)
+			flag, err := callAPI(lib.LibCode, isbn, authKeyList)
 			if err != nil {
 				return err
 			}
